e3e15ddec4a6d0e571dba76fec0fc8bf778bc13b
spinilla2828 <143560444+spinilla2828@users.noreply.github.com>
2023-11-21 17:22:57 -0500

    Add files via upload
    Segundo commit

--- logica/logica.js
+++ logica/logica.js
@@ -1,3 +1,15 @@
 function saludar(){
     alert("Hola como estan.")
 }
+
+function endpoint(){
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch("https://pokeapi.co/api/v2/pokemon/ditto", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
